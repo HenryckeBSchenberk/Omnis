399840e891ef55eeda266627f5760af7667656e6
HenryckeBSchenberk <henrycke.bozza@orakolo.com.br>
2023-01-19 14:06:37 +0000

    add object node and tests

---
+++ backend/src/nodes/object/object_node.py
@@ -0,0 +1,66 @@
+from src.nodes.node_manager import NodeManager
+from src.nodes.base_node import BaseNode
+from api import logger, exception
+from api.decorators import for_all_methods
+from src.nodes.object.object import Object, Manager as ObjectManager
+
+NODE_TYPE = "ObjectNode"
+
+
+@for_all_methods(exception(logger))
+class ObjectNode(BaseNode):
+    """
+        Esse nó possui duas interfaces, uma de entrada e outra de saida.
+        A interface de entrada é responsável por receber a alteração deseja ao objeto.
+        A interface de saida é responsável por enviar a alteração realizada no objeto.
+
+        O realiza as seguintes operações:
+         - Recebe e processa a alteração a ser feita.
+         - Atualiza os valores com base contexto do ObjectManager.
+         - Exporta as atualizações para o banco de dados (sync: True).
+         - Envia a alteração realizada para a interface de saída.
+
+        - Options:
+            - _id: Identificador do objeto a ser alterado.
+            - content: Conteúdo a ser alterado.
+
+    """
+
+    def __init__(self, name, id, options, output_connections, input_connections):
+        super().__init__(name, NODE_TYPE, id, options, output_connections)
+        self.target_id = options.get("_id", None)
+        # assert (self.target_id, "ObjectNode: Target id not defined.")
+
+        self.input_connections = input_connections
+        self.content = options.get("content", {})
+        self.sync = options.get("sync", False)
+
+        self.object = Object(self.target_id, **self.content)
+        NodeManager.addNode(self)
+
+    @BaseNode.Notify_Execution()
+    async def execute(self, message=""):
+        # assert(isinstance(message, dict), "ObjectNode: Message must be a dict.")
+
+        self.object.load(**message)
+        self.content = self.object.export()['content']
+        self.on("Gatilho", self.content)
+        if self.sync:
+            self.sync_content()
+
+    def sync_content(self):
+        ObjectManager.update(
+            _id=self.target_id,
+            input={
+                "content": self.content,
+            },
+            user=self.user
+        )
+
+    @staticmethod
+    def get_info(**kwargs):
+        return {
+            "options": {
+                "objects": list(ObjectManager.get_list(filter={"content":1})),
+            }
+        }
---
+++ backend/src/nodes/object/test/test_object_node.py
@@ -0,0 +1,51 @@
+import unittest
+from src.nodes.node_registry import NodeRegistry
+from src.nodes.object.object_node import ObjectNode, ObjectManager
+from src.utility.crud.user import User
+from bson import ObjectId as new_id
+import asyncio
+
+
+class TestObjectNode(unittest.TestCase):
+    def setUp(self):
+        _static_id = new_id()
+        self.user = User('omnis', 'test_case', 'developer', '')
+        self.object_id = ObjectManager.create(
+            input={"name": "default_name", "type": "object_test"}, _id=_static_id, user=self.user)
+        self.loop = asyncio.new_event_loop()
+
+    def test_ExecutionDataIntegrity(self):
+        node = ObjectNode("UnitTest_ObjectNode", self.object_id, {"_id": self.object_id, "content": {
+                          "name": "object_name", "type": "object_test"}}, [], [])
+
+        old_pointer = node.object.export()
+        old_contet = old_pointer['content'].copy()
+
+        executer = node.execute({"name": "updated_node"})
+        self.loop.run_until_complete(executer)
+        new_pointer = node.object.export()
+        new_content = new_pointer['content']
+
+        self.assertFalse(old_pointer is new_pointer)    # different object
+        # same content, because it's the content is the same, since is a internal dict updated.
+        self.assertEqual(old_pointer, new_pointer)
+
+        self.assertFalse(old_contet is new_content)    # different object
+        # different content, because old_content use ".copy()" to create a new dict.
+        self.assertTrue(old_contet != new_content)
+
+        # same content, because is a internal dict updated.
+        self.assertTrue(old_pointer['content'] == new_pointer['content'])
+
+    def test_ExecutionDataSyncDBO(self):
+        node = ObjectNode("UnitTest_ObjectNode", self.object_id, {"_id": self.object_id, "content": {
+                          "name": "object_name", "type": "object_test"}, "sync": True}, [], [])
+        excuter = node.execute({"name": "updated_node_sync"})
+        self.loop.run_until_complete(excuter)
+
+        get_node = ObjectManager.get_item(_id=self.object_id, user=self.user)
+        self.assertEqual(get_node['content'], node.object.export()['content'])
+
+    def tearDown(self):
+        self.loop.close()
+        ObjectManager.delete(_id=self.object_id, user=self.user)
